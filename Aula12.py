e8ceed638c2522a2e3803f1448720e3e1969dff3
JefferssonJanoski <janoskijeffersson025@gmail.com>
2024-07-10 18:14:03 -0300

    Aula 12 Udemy

---
+++ Aula12.py
@@ -0,0 +1,7 @@
+nome = input('Nome: ')
+altura = float(input('Altura: '))
+peso = float(input('Peso: '))
+
+imc = peso / (altura * altura)
+
+print(f'# {nome} tem {altura:.2f} de altura,\n# Pesa {peso:.2f} quilos e seu imc é\n# {imc:.2f}\n')
